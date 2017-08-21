7707ba280c7c6439b124914d9be3d48d86aca52b
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2017-08-21 22:04:53 +0900

    Add timezone conf

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RailsTutorial
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
